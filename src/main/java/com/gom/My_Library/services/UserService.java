41ccecb6cd7e0004476e87d78e1e1545584b4700
gomes <joaogomesleite800@gmail.com>
2025-04-08 12:44:03 -0300

    feat: create UserService

---
+++ src/main/java/com/gom/My_Library/services/UserService.java
@@ -0,0 +1,54 @@
+package com.gom.My_Library.services;
+
+import com.gom.My_Library.models.User;
+import com.gom.My_Library.repositories.UserRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> findAll() { return userRepository.findAll();}
+
+    public Optional<User> findById(Long id) {
+        return userRepository.findById(id);
+    }
+
+    public User insert(User obj) {
+        return userRepository.save(obj);
+    }
+
+    private void updateData(User entity, User obj) {
+        entity.setName(obj.getName());
+        entity.setLogin(obj.getLogin());
+    }
+
+    public User update(Long id, User obj) {
+        try {
+            User entity = userRepository.getReferenceById(id);
+            updateData(entity, obj);
+            return userRepository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new RuntimeException();
+        }
+    }
+
+    public void delete(Long id) {
+        try {
+            User user = userRepository.findById(id)
+                    .orElseThrow(() -> new EntityNotFoundException("User not found with ID: " + id));
+            userRepository.delete(user);
+        } catch (DataIntegrityViolationException e) {
+            throw new RuntimeException("Database integrity violation: " + e.getMessage());
+        }
+    }
+
+}
