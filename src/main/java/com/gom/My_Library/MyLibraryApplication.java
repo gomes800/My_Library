45e37c5662d75f6ccd953044f2b6382a055d9fab
gomes <joaogomesleite800@gmail.com>
2025-04-08 13:02:15 -0300

    feat: add FeignClient

--- src/main/java/com/gom/My_Library/MyLibraryApplication.java
+++ src/main/java/com/gom/My_Library/MyLibraryApplication.java
@@ -1,9 +1,12 @@
 package com.gom.My_Library;
 
+import jdk.jfr.Enabled;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class MyLibraryApplication {
 
 	public static void main(String[] args) {
