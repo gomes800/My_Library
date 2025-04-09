2bd62e2c6e837f85e5d59038be821f790fd876c3
gomes <joaogomesleite800@gmail.com>
2025-04-09 12:33:49 -0300

    feat: add UserDetails methods in User class ans findByLogin in UserRepository

--- src/main/java/com/gom/My_Library/models/User.java
+++ src/main/java/com/gom/My_Library/models/User.java
@@ -2,10 +2,14 @@ package com.gom.My_Library.models;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 @Entity
@@ -16,21 +20,53 @@ import java.util.List;
 @AllArgsConstructor
 @EqualsAndHashCode(of = "id")
 
-public class User implements Serializable {
+public class User implements Serializable, UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String login;
     private String password;
+    private UserRole role;
     private LocalDateTime createDate;
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<UserBook> bookList = new ArrayList<>();
+
 
     @PrePersist
     public void prePersist() {
         createDate = LocalDateTime.now();
     }
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        if (this.role == UserRole.ADMIN) return List.of(new SimpleGrantedAuthority("ROLE_ADMIN"), new SimpleGrantedAuthority("ROLE_USER"));
+        else return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getUsername() {
+        return login;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
--- src/main/java/com/gom/My_Library/repositories/UserRepository.java
+++ src/main/java/com/gom/My_Library/repositories/UserRepository.java
@@ -2,6 +2,9 @@ package com.gom.My_Library.repositories;
 
 import com.gom.My_Library.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    UserDetails findByLogin(String login);
 }
